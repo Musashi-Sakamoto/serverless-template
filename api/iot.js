693cc24a1c821272daddcae56a039aecc2584ac1
musashi.sakamoto <musashi.sakamoto@brilliantservice.co.jp>
2018-09-10 17:56:08 +0900

    iot->kinesis

--- api/iot.js
+++ api/iot.js
@@ -1,7 +1,24 @@
 'use strict';
+var AWS = require('aws-sdk'), dynamo = new AWS.DynamoDB.DocumentClient({
+    region: "ap-northeast-1"
+})
+var uuid = require('uuid').v4
 
 exports.handler = (event, context, callback) => {
-
+    console.log(`iot: ${JSON.stringify(event)}`)
+    dynamo.put({
+        "TableName": process.env.DYNAMODB_TABLE,
+        "Item": {
+            "id": uuid(),
+            "temperature": event.temperature.toString()
+        }
+    }, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('success')
+        }
+    })
     const response = {
         statusCode: 200,
         headers: {
--- api/process.js
+++ api/process.js
@@ -1,7 +1,29 @@
 'use strict';
 
-exports.handler = (event, context, callback) => {
+var AWS = require('aws-sdk'), dynamo = new AWS.DynamoDB.DocumentClient({
+    region: "ap-northeast-1"
+})
+var uuid = require('uuid').v4
 
+exports.handler = (event, context, callback) => {
+    event.Records.forEach(record => {
+        const payload = new Buffer(record.kinesis.data, 'base64').toString('ascii')
+        const object = JSON.parse(payload)
+        console.log(`decoded payload: ${payload}`)
+        dynamo.put({
+            "TableName": process.env.DYNAMODB_TABLE,
+            "Item": {
+                "id": uuid(),
+                "temperature": object.temperature.toString()
+            }
+        }, (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('yay')
+            }
+        })
+    });
     const response = {
         statusCode: 200,
         headers: {
